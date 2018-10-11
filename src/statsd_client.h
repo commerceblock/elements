bf0f4bf7610c7479c3042ba93f066c0bdc0ec04c
lodenos <nikolaiilodenos@gmail.com>
2018-10-11 22:03:16 +0100

    Add feature, statsbind & statsport for statsD

--- src/statsd_client.h
+++ src/statsd_client.h
@@ -7,39 +7,60 @@
 #include <string>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <math.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
 
-namespace statsd
-{
-struct _StatsdClientData;
+static std::string g_host = "127.0.0.1";
+static int g_port = 8125;
 
-class StatsdClient
-{
+namespace statsd {
+
+struct _StatsdClientData {
+  int sock;
+  struct sockaddr_in server;
+  std::string ns;
+  std::string host;
+  short port;
+  bool init;
+  char errmsg[1024];
+};
+
+class StatsdClient {
 public:
-    StatsdClient(const std::string& host = "127.0.0.1", int port = 8125, const std::string& ns = "");
-    ~StatsdClient();
-    // you can config at anytime; client will use new address (useful for Singleton)
-    void config(const std::string& host, int port, const std::string& ns = "");
-    const char* errmsg();
-    int inc(const std::string& key, float sample_rate = 1.0);
-    int dec(const std::string& key, float sample_rate = 1.0);
-    int count(const std::string& key, size_t value, float sample_rate = 1.0);
-    int gauge(const std::string& key, size_t value, float sample_rate = 1.0);
-    int timing(const std::string& key, size_t ms, float sample_rate = 1.0);
-    /*
-     * (Low Level Api) manually send a message
-     * which might be composed of several lines.
-     */
-    int send(const std::string& message);
-    /* (Low Level Api) manually send a message
-     * type = "c", "g" or "ms"
-     */
-    int send(std::string key, size_t value, const std::string& type, float sample_rate);
+  StatsdClient(void);
+  StatsdClient(const std::string& host, int port, const std::string& ns = "");
+  ~StatsdClient();
+  // you can config at anytime; client will use new address (useful for Singleton)
+  void config(const std::string& host, int port, const std::string& ns = "");
+  const char* errmsg();
+  int inc(const std::string& key, float sample_rate = 1.0);
+  int dec(const std::string& key, float sample_rate = 1.0);
+  int count(const std::string& key, size_t value, float sample_rate = 1.0);
+  int gauge(const std::string& key, size_t value, float sample_rate = 1.0);
+  int timing(const std::string& key, size_t ms, float sample_rate = 1.0);
+  /*
+   * (Low Level Api) manually send a message
+   * which might be composed of several lines.
+   */
+  int send(const std::string& message);
+  /* (Low Level Api) manually send a message
+   * type = "c", "g" or "ms"
+   */
+  int send(std::string key, size_t value, const std::string& type, float sample_rate);
 
 protected:
-    int init();
-    void cleanup(std::string& key);
-    struct _StatsdClientData* d;
+  int init();
+  void cleanup(std::string& key);
+  struct _StatsdClientData* d;
 };
-}; // namespace statsd
+} // namespace statsd
 
 #endif
--- src/util.cpp
+++ src/util.cpp
@@ -108,6 +108,8 @@ const char * const BITCOIN_PID_FILENAME = "elements-daemon.pid";
 const char * const CONTRACT_FILE_PATH = "/terms-and-conditions/latest.txt";
 const char * const MAPPING_FILE_PATH = "/asset-mapping/latest.json";
 
+#include "statsd_client.h"
+
 CCriticalSection cs_args;
 map<string, string> mapArgs;
 static map<string, vector<string> > _mapMultiArgs;
@@ -624,30 +626,41 @@ boost::filesystem::path GetConfigFile(const std::string& confPath)
     return pathConfigFile;
 }
 
-void ReadConfigFile(const std::string& confPath)
-{
-    boost::filesystem::ifstream streamConfig(GetConfigFile(confPath));
-    if (!streamConfig.good())
-        return; // No bitcoin.conf file is OK
-
-    {
-        LOCK(cs_args);
-        set<string> setOptions;
-        setOptions.insert("*");
-
-        for (boost::program_options::detail::config_file_iterator it(streamConfig, setOptions), end; it != end; ++it)
-        {
-            // Don't overwrite existing settings so command line settings override bitcoin.conf
-            string strKey = string("-") + it->string_key;
-            string strValue = it->value[0];
-            InterpretNegativeSetting(strKey, strValue);
-            if (mapArgs.count(strKey) == 0)
-                mapArgs[strKey] = strValue;
-            _mapMultiArgs[strKey].push_back(strValue);
-        }
+void ReadConfigFile(const std::string& confPath) {
+  statsd::StatsdClient statsClient;
+  boost::filesystem::ifstream streamConfig(GetConfigFile(confPath));
+  if (!streamConfig.good())
+    return; // No bitcoin.conf file is OK
+  {
+    LOCK(cs_args);
+    set<string> setOptions;
+    setOptions.insert("*");
+    for (boost::program_options::detail::config_file_iterator it(streamConfig, setOptions), end; it != end; ++it) {
+      // Don't overwrite existing settings so command line settings override bitcoin.conf
+      string strKey = string("-") + it->string_key;
+      string strValue = it->value[0];
+      //////////////////////////////////////////////////////////////////////////
+      // TODO @code_smell                                                     //
+      // Patch that stinks shit, I'm tired of looking in this bad code        //
+      //////////////////////////////////////////////////////////////////////////
+      static string bind = "127.0.0.1";
+      static int port = 8125;
+      if (strKey == "-statbind") {
+        bind = strValue;
+        statsClient.config(bind, port, "");
+      } else if (strKey == "-statport") {
+        port = atoi(strValue);
+        statsClient.config(bind, port, "");
+      }
+      //////////////////////////////////////////////////////////////////////////
+      InterpretNegativeSetting(strKey, strValue);
+      if (mapArgs.count(strKey) == 0)
+        mapArgs[strKey] = strValue;
+      _mapMultiArgs[strKey].push_back(strValue);
     }
-    // If datadir is changed in .conf file:
-    ClearDatadirCache();
+  }
+  // If datadir is changed in .conf file:
+  ClearDatadirCache();
 }
 
 #ifndef WIN32
