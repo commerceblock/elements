a95aaa478683d02ebd2d304aa8ad0c70c88b2ad3
Lawrence Deacon <lawrence.deacon@gmail.com>
2019-11-07 11:40:16 +0000

    GetPreimage() function in CHashWriter (for debugging)

--- src/hash.h
+++ src/hash.h
@@ -30,6 +30,8 @@ public:
         sha.Reset().Write(buf, CSHA256::OUTPUT_SIZE).Finalize(hash);
     }
 
+    void 
+
     CHash256& Write(const unsigned char *data, size_t len) {
         sha.Write(data, len);
         return *this;
@@ -131,6 +133,8 @@ class CHashWriter
 {
 private:
     CHash256 ctx;
+    //For debugging purposes
+    std::vector<unsigned char> preimage;
 
     const int nType;
     const int nVersion;
@@ -143,6 +147,7 @@ public:
 
     void write(const char *pch, size_t size) {
         ctx.Write((const unsigned char*)pch, size);
+        preimage.insert(preimage.end(), pch, pch+size);
     }
 
     // invalidates the object
@@ -150,6 +155,10 @@ public:
         uint256 result;
         ctx.Finalize((unsigned char*)&result);
         return result;
+    }
+
+    std::vector<unsigned char> GetPreimage() const{
+        return preimage;
     }
 
     template<typename T>
