d7f578e80eb6f597967f7f2dd267fe8d8cdb8577
tomossomot <tomos@commerceblock.com>
2019-12-04 16:08:34 +0000

    basic rpc test

--- qa/rpc-tests/blockchain.py
+++ qa/rpc-tests/blockchain.py
@@ -26,38 +26,39 @@ class BlockchainTest(BitcoinTestFramework):
     """
     Test blockchain-related RPC calls:
 
-        - gettxoutsetinfo
+        - gettxoutsetinfo (depreciated)
         - verifychain
+        - getsidechaininfo
 
     """
 
     def __init__(self):
         super().__init__()
         self.setup_clean_chain = False
-        self.num_nodes = 2
+        self.num_nodes = 1
 
     def setup_network(self, split=False):
         self.nodes = start_nodes(self.num_nodes, self.options.tmpdir)
-        connect_nodes_bi(self.nodes, 0, 1)
         self.is_network_split = False
         self.sync_all()
 
     def run_test(self):
-        self._test_gettxoutsetinfo()
+        # self._test_gettxoutsetinfo()
         self._test_getblockheader()
+        self._test_getsidechaininfo()
         self.nodes[0].verifychain(4, 0)
 
-    def _test_gettxoutsetinfo(self):
-        node = self.nodes[0]
-        res = node.gettxoutsetinfo()
-
-        assert_equal(res['total_amount'], Decimal('8725.00000000'))
-        assert_equal(res['transactions'], 200)
-        assert_equal(res['height'], 200)
-        assert_equal(res['txouts'], 200)
-        assert_equal(res['bytes_serialized'], 13924),
-        assert_equal(len(res['bestblock']), 64)
-        assert_equal(len(res['hash_serialized']), 64)
+    # def _test_gettxoutsetinfo(self):
+    #     node = self.nodes[0]
+    #     res = node.gettxoutsetinfo()
+    #
+    #     assert_equal(res['total_amount'], Decimal('8725.00000000'))
+    #     assert_equal(res['transactions'], 200)
+    #     assert_equal(res['height'], 200)
+    #     assert_equal(res['txouts'], 200)
+    #     assert_equal(res['bytes_serialized'], 13924),
+    #     assert_equal(len(res['bestblock']), 64)
+    #     assert_equal(len(res['hash_serialized']), 64)
 
     def _test_getblockheader(self):
         node = self.nodes[0]
@@ -84,6 +85,19 @@ class BlockchainTest(BitcoinTestFramework):
         assert isinstance(header['version'], int)
         assert isinstance(int(header['versionHex'], 16), int)
         assert isinstance(header['difficulty'], int)#always 1
+
+    def _test_getsidechaininfo(self):
+        node = self.nodes[0]
+        info = node.getsidechaininfo()
+
+        assert_equal(int(info['fedpegscript']), 51)
+        assert_is_hex_string(info['pegged_asset'])
+        assert_is_hex_string(info['min_peg_diff'])
+        assert_is_hex_string(info['parent_blockhash'])
+        assert_equal(int(info['addr_prefixes']['PUBKEY_ADDRESS']),235)
+        assert_equal(int(info['addr_prefixes']['BLINDED_ADDRESS']),4)
+        assert_equal(int(info['addr_prefixes']['SECRET_KEY']),239)
+        assert_equal(int(info['addr_prefixes']['SCRIPT_ADDRESS']),75)
 
 if __name__ == '__main__':
     BlockchainTest().main()
