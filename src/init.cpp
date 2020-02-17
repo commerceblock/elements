ae5fb36797e80c6023ba6c62a72ddb038554dbc2
nkostoulas <nkostoulas@gmail.com>
2020-02-17 14:27:03 +0000

    Change ocean versioning

--- src/init.cpp
+++ src/init.cpp
@@ -564,7 +564,7 @@ std::string LicenseInfo()
     const std::string URL_SOURCE_CODE = "<https://github.com/commerceblock/ocean>";
     const std::string URL_WEBSITE = "<https://www.commerceblock.com>";
 
-    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2015, COPYRIGHT_YEAR) + " ") + "\n" +
+    return CopyrightHolders(strprintf(_("Copyright (C) %i-%i"), 2018, COPYRIGHT_YEAR) + " ") + "\n" +
            "\n" +
            strprintf(_("Please contribute if you find %s useful. "
                        "Visit %s for further information about the software."),
